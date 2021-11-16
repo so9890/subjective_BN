5594adc937b68059736672b038c257c802b3bb07
Sonja <sonja.dobkowitz@barcelonagse.eu>
2021-11-16 15:43:52 +0100

    model equations and matlab

--- codding_model/model_simple_PerfectComp.m
+++ codding_model/model_simple_PerfectComp.m
@@ -1,5 +1,13 @@
-%% Model file of the simple model
+%% Model file of the simple, static model
 % no innovation 
 % no monopolistic competition
+% no labour sector choice
 
-syms 
+% variables
+syms cnr cnp csr csp hh hl yn ys lls lln lhs lhn ...
+    xis xin pis pin psL pnL ps pn wh wl Ls Ln real
+
+% parameters
+syms eppsilons eppsilonn omegaa alphaa psii real
+
+%- model equations
