e36a680bcb8a1b062fa8a28fd9f9fa6d589d1af6
Sonja <sonja.dobkowitz@barcelonagse.eu>
2021-11-16 21:10:02 +0100

    matlab

--- codding_model/model_simple_PerfectComp.m
+++ codding_model/model_simple_PerfectComp.m
@@ -4,10 +4,102 @@
 % no labour sector choice
 
 % variables
-syms cnr cnp csr csp hh hl yn ys lls lln lhs lhn ...
-    xis xin pis pin psL pnL ps pn wh wl Ls Ln real
+syms cnh cnl csh csl Ch Cl hh hl yn ys lls lln lhs lhn ...
+     psL pnL ps pn wh wl Ls Ln real
 
+ % vector of variables for which to solve model
+ symvariables=[cnh, cnl, csh, csl, Ch, Cl, hh, hl, yn,ys, lls, lln, lhs, lhn, ...
+     psL, pnL, ps, pn, wh, wl, Ls, Ln];
 % parameters
-syms eppsilons eppsilonn omegaa alphaa psii real
+syms eppsilons eppsilonn omegas alphaa psii B lambdaa As An real
 
-%- model equations
+%-- model equations
+c=0;
+% HHs
+
+%high skill
+c=c+1;
+f(c)=csh-wh^2*(omegas/ps)^(1+omegas)*((1-omegas)/pn)^(1-omegas)/(1+wh^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^(2*(1-omegas)))*B;
+c=c+1;
+f(c)=cnh-wh^2*(omegas/ps)^(omegas)*((1-omegas)/pn)^(2-omegas)/(1+wh^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^((1-2*omegas)))*B;
+c=c+1;
+f(c)=hh- wh*(omegas/ps)^(omegas)*((1-omegas)/pn)^(1-omegas)*(1-(wh^2*(omegas/ps)^(omegas)*((1-omegas)/pn)^(1-omegas))...
+    /(1+wh^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^(1-2*omegas)))*B;
+c=c+1;
+f(c)=Ch-csh^omegas*cnh^(1-omegas);
+
+% low skill
+c=c+1;
+f(c)=csl-wl^2*(omegas/ps)^(1+omegas)*((1-omegas)/pn)^(1-omegas)/(1+wl^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^(2*(1-omegas)))*B;
+c=c+1;
+f(c)=cnl-wl^2*(omegas/ps)^(omegas)*((1-omegas)/pn)^(2-omegas)/(1+wl^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^((1-2*omegas)))*B;
+c=c+1;
+f(c)=hl- wl*(omegas/ps)^(omegas)*((1-omegas)/pn)^(1-omegas)*(1-(wl^2*(omegas/ps)^(omegas)*((1-omegas)/pn)^(1-omegas))...
+    /(1+wl^2*(omegas/ps)^(2*omegas)*((1-omegas)/pn)^(1-2*omegas)))*B;
+c=c+1;
+f(c)=Cl-csl^omegas*cnl^(1-omegas);
+
+% Production 
+
+% production labour input good
+c=c+1;
+f(c)=Ls-lhs^eppsilons*lls^(1-eppsilons);
+c=c+1;
+f(c)=Ln-lhn^eppsilonn*lln^(1-eppsilonn);
+
+% optimality labour producing firm
+c=c+1;
+f(c)=lhs-(psL*eppsilons/wh)^(1/(1-eppsilons))*lls;
+c=c+1;
+f(c)=lhn-(pnL*eppsilonn/wh)^(1/(1-eppsilonn))*lln;
+
+lls=(psL*(1-eppsilons)/wl)^(1/(eppsilons))*lhs;
+lln=(pnL*(1-eppsilonn)/wh)^(1/(eppsilonn))*lhn;
+
+% production final good
+c=c+1;
+f(c)=ys-alphaa*(ps/psii)^(alphaa/(1-alphaa))*As*Ls;
+c=c+1;
+f(c)=yn- alphaa*(pn/psii)^(alphaa/(1-alphaa))*An*Ln;
+
+% labour input demand => price for labour good
+c=c+1;
+f(c)=psL-(1-alphaa)*(alphaa/psii)^(alphaa/(1-alphaa))*ps^(1/(1-alphaa))*As;
+c=c+1;
+f(c)=pnL- (1-alphaa)*(alphaa/psii)^(alphaa/(1-alphaa))*pn^(1/(1-alphaa))*An;
+
+% machine demand
+% => already replaced!
+%xis=(alphaa*ps/pis)^(1/(1-alphaa))*Ls*Ais
+
+% optimality machine producers=> already replaced: pis=pin=psii
+
+% Market clearing
+
+% sust final good
+c=c+1;
+f(c)=ys-( lambdaa*csh+(1-lambdaa)*csl);
+
+% unsust final good => walras' law
+c=c+1;
+f(c)=pn-1;
+
+% high skill labour market
+c=c+1;
+f(c)=hh-(lhs+lhn);
+% low skill labour market
+c=c+1;
+f(c)=hl-(lls+lln);
+
+% labour input good
+c=c+1;
+f(c)=psL-1;
+
+% fixing wage low skill
+c=c+1;
+f(c)=wl-1;
+
+%--end model block
+
+%-- Solve symbolically
+S=solve(f, symvariables);
