55a912be3e47aea17314267a541dcf6dbcf9d0d8
Sonja <sonja.dobkowitz@barcelonagse.eu>
2022-01-02 10:31:09 +0100

    graphs evolution

--- codding_data/codes/overconsumption/3_liss_env_incomeWaves.py
+++ codding_data/codes/overconsumption/3_liss_env_incomeWaves.py
@@ -25,7 +25,7 @@ df = pd.read_stata(dtafile)
 
 """ read in and merge data on income. 
 
-    - only keep variables relevant to answer the question
+    - only keep variables relevant to answer the question in income dataset
     - ensure appending correct variables to one another (changes in variable names)
 
 """
@@ -79,6 +79,7 @@ data_long=pd.concat(list(dic_data.values()), join="outer")
 helpper = data_long.pop('year') # removes column b from frame and saves to new dataframe
 data_long.insert(loc = 1, column = 'year', value = list(helpper))
 
+
 """ Plot variables over time by environmental indicator. 
 
     for categorical variables: percentage not necessary
@@ -89,6 +90,12 @@ data_long.insert(loc = 1, column = 'year', value = list(helpper))
 # save to dictionary
 
 cons=['ci306', 'ci307']
+
+# dictionary with meaning of each variable
+meaning=['Do you buy new clothes regularly?', \
+         'Do you replace worn furniture with new furniture?']
+dicc=dict(zip(cons, meaning))    
+
 for i in cons:
    data_long[i+'_cat'] = data_long[i].astype('category').cat.codes
    # no i dont think it is necessary is encoded as 1
@@ -99,38 +106,97 @@ helper=data_long[~pd.isna(data_long['ci307'])]
 helper.ci306_cat[helper.ci306_cat!=1]=0
 helper.ci307_cat[helper.ci307_cat!=1]=0
 
-# collapse dataset by willingness to change
-data_col=helper[['qk20a175', 'year', 'ci306_cat', 'ci307_cat']].groupby(['qk20a175', 'year'], as_index=False).sum()
-total=helper[['qk20a175', 'year', 'ci306_cat', 'ci307_cat']].groupby(['qk20a175', 'year'], as_index=False).size()
-data_col=data_col.merge(total[['qk20a175', 'year','size']],  left_on= ['qk20a175', 'year'], right_on= ['qk20a175', 'year'], how= 'left', validate='1:1', indicator='source')
+# drop 2008-2010 as no observation from environmental dataset in panel
+helper=helper[helper.year.astype(float)>2010]
 
-#generate share for both variables
-for i in cons:
-    data_col[i+'share']=data_col[i+'_cat'].values/data_col['size']
- 
+""" collapse data set by distinct env. variable indicators. """
 
-# dictionary with meaning of each variable
-meaning=['Do you buy new clothes regularly?', \
-         'Do you replace worn furniture with new furniture?']
-dicc=dict(zip(cons, meaning))    
-
-# list of categories in grouping variable
-groups=list(data_col.qk20a175.drop_duplicates())
-
-# list of dataframes split
-frames=[data_col[data_col.qk20a175==groups[0]]]
-for s in range(1,len(groups)):
-    frames.append(data_col[data_col.qk20a175==groups[s]])
+for v in ['qk20a175', 'qk20a135', 'qk20a181', 'qk20a183', 'qk20a144', 'qk20a148']:
     
+    # collapse dataset by willingness to change
+    data_col=helper[[v, 'year', 'ci306_cat', 'ci307_cat']].groupby([v, 'year'], as_index=False).sum()
+    total=helper[[v, 'year', 'ci306_cat', 'ci307_cat']].groupby([v, 'year'], as_index=False).size()
+    data_col=data_col.merge(total[[v, 'year','size']],  left_on= [v, 'year'], right_on= [v, 'year'], how= 'left', validate='1:1', indicator='source')
     
+    #generate share for both variables
+    for i in cons:
+        data_col[i+'share']=data_col[i+'_cat'].values/data_col['size']
+     
+    
+    # list of categories in grouping variable
+    groups=list(data_col[v].drop_duplicates())
+    
+    # list of dataframes split
+    frames=[data_col[data_col[v]==groups[0]]]
+    for s in range(1,len(groups)):
+        frames.append(data_col[data_col[v]==groups[s]])
+        
+        
     # plot time series
-for i in cons:
-    fig = plt.figure()
-    for frame in frames:
-        plt.plot(frame['year'], frame[i+'share'])
-    plt.legend(groups)
-    plt.title(dicc[i])
-    plt.show()
-
-   # plt.savefig('../../results/liss/conditional_heatmap'+i+'_'+j+'labels'+str(p)+'.png', format='png', bbox_inches='tight')
-    #plt.clf()
+    for i in cons:
+        fig = plt.figure()
+        for frame in frames:
+            plt.plot(frame['year'], frame[i+'share'])
+        plt.legend(groups)
+        plt.title(dicc[i])
+        #plt.show()
+    
+        plt.savefig('../../results/liss/share_notnecessary'+v+'_'+i+'.png', format='png', bbox_inches='tight')
+        plt.clf()
+        
+    """ plot total"""
+    
+    # total share evolution
+    data_total=helper[[ 'year', 'ci306_cat', 'ci307_cat']].groupby(['year'], as_index=False).sum()
+    total_all=helper[[ 'year', 'ci306_cat', 'ci307_cat']].groupby(['year'], as_index=False).size()
+    data_total=data_total.merge(total_all[[ 'year','size']],  left_on= ['year'], right_on= [ 'year'], how= 'left', validate='1:1', indicator='source')
+    
+    # generate share for both variables
+    for i in cons:
+        data_total[i+'share']=data_total[i+'_cat'].values/data_total['size']
+    
+        fig = plt.figure()
+        plt.plot(data_total.year, data_total[i+'share'])
+        plt.title(dicc[i])
+        plt.legend(['no, not necessary (in percent)'])
+        #plt.show()
+        plt.savefig('../../results/liss/total_share_notnecessary'+v+'_'+i+'.png', format='png', bbox_inches='tight')
+        plt.clf()
+        
+    """ plot broader categories"""
+    
+    # only two groups agree and disagree
+    helper['group_broad']='Others'
+    helper.group_broad[(helper[v]=='Completely disagree') | (helper[v]=='Disagree')]='Disagree'
+    helper.group_broad[(helper[v]=='Completely agree') | (helper[v]=='Agree')]='Agree'
+    
+    # collapse dataset by willingness to change
+    data_col=helper[['group_broad', 'year', 'ci306_cat', 'ci307_cat']].groupby(['group_broad', 'year'], as_index=False).sum()
+    total=helper[['group_broad', 'year', 'ci306_cat', 'ci307_cat']].groupby(['group_broad', 'year'], as_index=False).size()
+    data_col=data_col.merge(total[['group_broad', 'year','size']],  left_on= ['group_broad', 'year'], right_on= ['group_broad', 'year'], how= 'left', validate='1:1', indicator='source')
+    
+    #generate share for both variables
+    for i in cons:
+        data_col[i+'share']=data_col[i+'_cat'].values/data_col['size']
+     
+    # list of categories in grouping variable
+    groups=list(data_col.group_broad.drop_duplicates())
+    
+    # list of dataframes split
+    frames=[data_col[data_col.group_broad==groups[0]]]
+    for s in range(1,len(groups)):
+        frames.append(data_col[data_col.group_broad==groups[s]])
+        
+        
+    # plot time series
+    for i in cons:
+        fig = plt.figure()
+        for frame in frames:
+            plt.plot(frame['year'], frame[i+'share'])
+        plt.legend(groups)
+        plt.title(dicc[i]+': no, not necessary (in percent)')
+        #plt.show()
+    
+        plt.savefig('../../results/liss/broad_groups_notnecessary'+v+'_'+i+'.png', format='png', bbox_inches='tight')
+        plt.clf()
+    
